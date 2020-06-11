ab11918a05d502e77f8ec98c31c64fe63ca345be
jhonatan estevam <jhonatan229@outlook.com>
2020-06-11 15:13:52 -0300

    create clas playment ans assosiation with order - OneToOne

--- src/main/java/com/jhonatanEL/course/config/TestConfig.java
+++ src/main/java/com/jhonatanEL/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jhonatanEL.course.entites.Category;
 import com.jhonatanEL.course.entites.Order;
 import com.jhonatanEL.course.entites.OrderItem;
+import com.jhonatanEL.course.entites.Payment;
 import com.jhonatanEL.course.entites.Product;
 import com.jhonatanEL.course.entites.User;
 import com.jhonatanEL.course.entites.enums.OrderStatus;
@@ -83,5 +84,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 	
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T20:53:07Z") , o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
