752f7be8ab88245843793eb07f1728b16135c383
jhonatan estevam <jhonatan229@outlook.com>
2020-06-11 16:30:06 -0300

    user insert

--- src/main/java/com/jhonatanEL/course/resources/UserResources.java
+++ src/main/java/com/jhonatanEL/course/resources/UserResources.java
@@ -1,13 +1,17 @@
 package com.jhonatanEL.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jhonatanEL.course.entites.User;
 import com.jhonatanEL.course.services.UserService;
@@ -15,21 +19,30 @@ import com.jhonatanEL.course.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
-	 
+
 	@Autowired
-	private UserService service ;
-	
-	//mostra os objetos exixtentes dentro do db, atraves da url users
+	private UserService service;
+
+	// mostra os objetos exixtentes dentro do db, atraves da url users
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
-	//mostra o objeto de id x, colocado no url dps de users
+
+	// mostra o objeto de id x, colocado no url dps de users
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	// a anotação usada para inserir elementos
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
